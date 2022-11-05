4cab7346eaea80e5e5e9afc62d16508fb89dc80a
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-05 23:28:31 +0300

    optimize code

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,10 +2,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
 
-  def actor
-    Actor.find(self.actor_id)
-    # .characters.actor
-  end
   def say_that_thing_you_say
   "#{self.name} always says: #{self.catchphrase}"
     end
